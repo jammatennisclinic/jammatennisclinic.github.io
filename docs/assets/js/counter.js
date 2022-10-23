e678f71a07ba3aad26efc070fa5e2df36a43f865
minisounds <31378516+minisounds@users.noreply.github.com>
2022-10-23 16:16:27 -0700

    Change to $70,000

--- docs/assets/js/counter.js
+++ docs/assets/js/counter.js
@@ -3,22 +3,34 @@ console.log("counter being called");
 const counters = document.querySelectorAll('.counter');
 const speed = 200;
 
-counters.forEach(counter => {
-    const updateCount = () => {
-        console.log("inside counters function")
-        const target = +counter.getAttribute('data-target');
-        const count = +counter.innerText;
-        const inc = target / speed;
-        console.log(target);
-        console.log(count);
-        console.log(inc);
-        if(count < target) {
-            const value = count + inc;
-            counter.innerText = value;
-            setTimeout(updateCount, 1);
-        } else {
-            count.innerText = parseInt(target);
-        }
+window.addEventListener('scroll', () => {
+    const scrolled = window.offsetTop;
+    const elementPosition = counters.offsetTop;
+    
+    if(scrolled == elementPosition){
+        startCount();
     }
-    updateCount();
-});
+})
+
+function startCount() {
+    counters.forEach(counter => {
+        const updateCount = () => {
+            console.log("inside counters function")
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const inc = target / speed;
+            console.log(target);
+            console.log(count);
+            console.log(inc);
+            if(count < target) {
+                const value = count + inc;
+                counter.innerText = value;
+                setTimeout(updateCount, 1);
+            } else {
+                count.innerText = parseInt(target);
+            }
+        }
+        updateCount();
+    });
+}
+
