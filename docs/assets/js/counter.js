9595032dcc0a47c3646879c995c9790f9c5a1bd3
minisounds <31378516+minisounds@users.noreply.github.com>
2023-01-06 21:16:34 -1000

    Added Delay to Numbers Loading

--- docs/assets/js/counter.js
+++ docs/assets/js/counter.js
@@ -1,27 +1,34 @@
 console.log("counter being called");
 
 const counters = document.querySelectorAll('.counter');
-const speed = 200;
+const speed = 50;
 
 window.addEventListener('scroll', () => {
-    const scrolled = window.offsetTop;
-    const elementPosition = counters.offsetTop;
-    
-    if(scrolled == elementPosition){
+    const firstNumber = document.getElementById('firstNumber');
+
+    if (firstNumber.getBoundingClientRect().top >= 200 && firstNumber.getBoundingClientRect().bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+        console.log(firstNumber.getBoundingClientRect().top);
         startCount();
     }
+
+    // const scrolled = window.offsetTop;
+    // const elementPosition = counters.offsetTop;
+    
+    // if(scrolled == elementPosition){
+    //     startCount();
+    // }
 })
 
 function startCount() {
     counters.forEach(counter => {
         const updateCount = () => {
-            console.log("inside counters function")
+            // console.log("inside counters function")
             const target = +counter.getAttribute('data-target');
             const count = +counter.innerText;
             const inc = target / speed;
-            console.log(target);
-            console.log(count);
-            console.log(inc);
+            // console.log(target);
+            // console.log(count);
+            // console.log(inc);
             if(count < target) {
                 const value = count + inc;
                 counter.innerText = value;
